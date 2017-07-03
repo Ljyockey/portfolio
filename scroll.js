d23582def9bf302d0031f8682604107d807f701c
ljyockey <linyockey@gmail.com>
2017-07-02 21:29:05 -0700

    Made changes per EJ's notes

--- scroll.js
+++ scroll.js
@@ -1,12 +1,17 @@
 //smooth scrolling
 function smoothScroll() {
-	$('.scroll').on('click', function(e) {
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top-50}, 800);
-	});
+  $('.scroll').on('click', function(e) {
+    e.preventDefault();
+	var hash = $(this.hash);
+    $('html, body').animate({
+      scrollTop: hash.offset().top-50}, 800);
+	  hash.addClass('flash');
+	  setTimeout(function() {
+      	hash.removeClass('flash');
+    }, 500);
+  });
 }
 
 $(function() {
-	smoothScroll();
+  smoothScroll();
 });
