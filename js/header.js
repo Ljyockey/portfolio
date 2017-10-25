c8f1a973b715b2107866b497e7cbdd67d138368f
ljyockey <linyockey@gmail.com>
2017-10-24 19:52:07 -0700

    displayTitles function

---
+++ js/header.js
@@ -0,0 +1,22 @@
+var titles = [
+  'Sports Fan',
+  'Theme Park Enthusiast',
+  'Tweets with GIFs',
+  'Full-Stack Web Developer'  
+];
+
+function displayTitles() {
+	var count = 0;
+	setInterval(function() {
+		$('.js-titles').css({fontSize: '0'}).text(titles[count]).animate({fontSize: '48px'});
+		count++;
+		if (count == titles.length) {
+			count = 0;
+		}
+	}, 3000);
+
+}
+
+$(function() {
+	displayTitles();
+});
